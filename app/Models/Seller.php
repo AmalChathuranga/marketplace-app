fa777ad4d46d7da5a17df1c23b1c31281d976c2e
AmalChathuranga <amalcodochathuranga@gmail.com>
2024-01-19 17:16:29 +0530

    feat:migration and model relationship done

---
+++ app/Models/Seller.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Seller extends Authenticatable
+{
+    use HasFactory, HasFactory, Notifiable;
+
+    protected $fiiable =['name','email','password','address','contact_no'];
+
+    protected $hidden = [
+        'password',
+     
+    ];
+
+    protected $casts = [
+       
+        'password' => 'hashed',
+    ];
+
+    //product relationship
+
+    public function products() :BelongsToMany
+    {
+        return $this->belongsToMany(Product::class,'seller_products','seller_id','product_id');
+    }
+}
